474b8d0d9bcbbf8664d65b27d6ecf074c53f8d95
prashanthKumar0 <31966594+PrashanthKumar0@users.noreply.github.com>
2023-05-12 22:50:02 +0530

    bitmap test

--- DinoGame.ino
+++ DinoGame.ino
@@ -1,20 +1,39 @@
-#include <Adafruit_GFX.h>    // Core graphics library
-#include <Adafruit_ST7735.h> // Hardware-specific library for ST7735
+#include <Adafruit_GFX.h>     // Core graphics library
+#include <Adafruit_ST7735.h>  // Hardware-specific library for ST7735
 #include <SPI.h>
 
-#define TFT_CS        D1 // PyBadge/PyGamer display control pins: chip select
-#define TFT_RST       D0 // Display reset
-#define TFT_DC        D2 // Display data/command select
-#define TFT_BACKLIGHT D3 // Display backlight pin
+#define TFT_CS D1   // PyBadge/PyGamer display control pins: chip select
+#define TFT_RST D0  // Display reset
+#define TFT_DC D2   // Display data/command select
 
 Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_RST);
 
-float p = 3.1415926;
+constexpr float pi = 3.1415926;
+
+/*------------------------------
+||      GLOBAL  DECLARATIONS  ||
+-------------------------------*/
+
+const uint8_t buffer[]={
+  0b00110000,
+  0b11110000,
+  0b00110010,
+  0b00110000,
+  0b00110000,
+  0b00110000,
+  0b11111111,
+  0b11111111, 
+};
 
 void setup(void) {
-  tft.drawLine(0,0,10,10,0xFFFF); // white line  
+  tft.initR(INITR_BLACKTAB);
+  tft.setRotation(1);
+  clear(); 
+  tft.drawBitmap(0, 0, (uint8_t*)buffer, 8, 8,0xFFFF);
 }
 
 void loop() {
-
+}
+void clear() {
+  tft.fillScreen(0);
 }
---
+++ tools/main.js
@@ -0,0 +1,241 @@
+onload = main;
+
+/**
+ * constatns from chromium
+ * 
+ */
+const spriteData = {
+    LDPI: {
+        BACKGROUND_EL: { x: 86, y: 2 },
+        CACTUS_LARGE: { x: 332, y: 2 },
+        CACTUS_SMALL: { x: 228, y: 2 },
+        OBSTACLE_2: { x: 332, y: 2 },
+        OBSTACLE: { x: 228, y: 2 },
+        CLOUD: { x: 86, y: 2 },
+        HORIZON: { x: 2, y: 54 },
+        MOON: { x: 484, y: 2 },
+        PTERODACTYL: { x: 134, y: 2 },
+        RESTART: { x: 2, y: 68 },
+        TEXT_SPRITE: { x: 655, y: 2 },
+        TREX: { x: 848, y: 2 },
+        STAR: { x: 645, y: 2 },
+        COLLECTABLE: { x: 2, y: 2 },
+        ALT_GAME_END: { x: 121, y: 2 },
+    },
+    HDPI: {
+        BACKGROUND_EL: { x: 166, y: 2 },
+        CACTUS_LARGE: { x: 652, y: 2 },
+        CACTUS_SMALL: { x: 446, y: 2 },
+        OBSTACLE_2: { x: 652, y: 2 },
+        OBSTACLE: { x: 446, y: 2 },
+        CLOUD: { x: 166, y: 2 },
+        HORIZON: { x: 2, y: 104 },
+        MOON: { x: 954, y: 2 },
+        PTERODACTYL: { x: 260, y: 2 },
+        RESTART: { x: 2, y: 130 },
+        TEXT_SPRITE: { x: 1294, y: 2 },
+        TREX: { x: 1678, y: 2 },
+        STAR: { x: 1276, y: 2 },
+        COLLECTABLE: { x: 4, y: 4 },
+        ALT_GAME_END: { x: 242, y: 4 },
+    },
+    MAX_GAP_COEFFICIENT: 1.5,
+    MAX_OBSTACLE_LENGTH: 3,
+    HAS_CLOUDS: 1,
+    BOTTOM_PAD: 10,
+    TREX: {
+        WAITING_1: { x: 44, w: 44, h: 47, xOffset: 0 },
+        WAITING_2: { x: 0, w: 44, h: 47, xOffset: 0 },
+        RUNNING_1: { x: 88, w: 44, h: 47, xOffset: 0 },
+        RUNNING_2: { x: 132, w: 44, h: 47, xOffset: 0 },
+        JUMPING: { x: 0, w: 44, h: 47, xOffset: 0 },
+        CRASHED: { x: 220, w: 44, h: 47, xOffset: 0 },
+        COLLISION_BOXES: [
+            new CollisionBox(22, 0, 17, 16),
+            new CollisionBox(1, 18, 30, 9),
+            new CollisionBox(10, 35, 14, 8),
+            new CollisionBox(1, 24, 29, 5),
+            new CollisionBox(5, 30, 21, 4),
+            new CollisionBox(9, 34, 15, 4),
+        ],
+    },
+    OBSTACLES: [
+        {
+            type: 'CACTUS_SMALL',
+            width: 17,
+            height: 35,
+            yPos: 105,
+            multipleSpeed: 4,
+            minGap: 120,
+            minSpeed: 0,
+            collisionBoxes: [
+                new CollisionBox(0, 7, 5, 27),
+                new CollisionBox(4, 0, 6, 34),
+                new CollisionBox(10, 4, 7, 14),
+            ],
+        },
+        {
+            type: 'CACTUS_LARGE',
+            width: 25,
+            height: 50,
+            yPos: 90,
+            multipleSpeed: 7,
+            minGap: 120,
+            minSpeed: 0,
+            collisionBoxes: [
+                new CollisionBox(0, 12, 7, 38),
+                new CollisionBox(8, 0, 7, 49),
+                new CollisionBox(13, 10, 10, 38),
+            ],
+        },
+        {
+            type: 'PTERODACTYL',
+            width: 46,
+            height: 40,
+            yPos: [100, 75, 50],    // Variable height.
+            yPosMobile: [100, 50],  // Variable height mobile.
+            multipleSpeed: 999,
+            minSpeed: 8.5,
+            minGap: 150,
+            collisionBoxes: [
+                new CollisionBox(15, 15, 16, 5),
+                new CollisionBox(18, 21, 24, 6),
+                new CollisionBox(2, 14, 4, 3),
+                new CollisionBox(6, 10, 4, 7),
+                new CollisionBox(10, 8, 6, 9),
+            ],
+            numFrames: 2,
+            frameRate: 1000 / 6,
+            speedOffset: .8,
+        },
+    ],
+    BACKGROUND_EL: {
+        'CLOUD': {
+            HEIGHT: 14,
+            MAX_CLOUD_GAP: 400,
+            MAX_SKY_LEVEL: 30,
+            MIN_CLOUD_GAP: 100,
+            MIN_SKY_LEVEL: 71,
+            OFFSET: 4,
+            WIDTH: 46,
+            X_POS: 1,
+            Y_POS: 120,
+        },
+    },
+    BACKGROUND_EL_CONFIG: {
+        MAX_BG_ELS: 1,
+        MAX_GAP: 400,
+        MIN_GAP: 100,
+        POS: 0,
+        SPEED: 0.5,
+        Y_POS: 125,
+    },
+    LINES: [
+        { SOURCE_X: 2, SOURCE_Y: 52, WIDTH: 600, HEIGHT: 12, YPOS: 127 },
+    ],
+};
+
+
+
+
+
+let img;
+let ctx;
+const W = 160;
+const H = 128;
+
+const scaleFactor = 1; // 600 is original game width
+
+function CollisionBox(x, y, w, h) {
+    this.x = x;
+    this.y = y;
+    this.w = w;
+    this.h = h;
+}
+
+function main() {
+    ctx = $("#cnvs").getContext('2d');
+    ctx.imageSmoothingEnabled = false;
+    img = new Image();
+    img.src = "./assets/sprite_low.png";
+    img.onload = setup;
+}
+
+function setup() {
+    getDinoWaitingImage();
+}
+
+function getDinoWaitingImage() {
+    let x = spriteData.LDPI.TREX.x;
+    let y = spriteData.LDPI.TREX.y;
+    let w = spriteData.TREX.WAITING_1.w;
+    let h = spriteData.TREX.WAITING_1.h;
+    ctx.canvas.width = Math.round(w * scaleFactor); // clears canvas
+    ctx.canvas.height = Math.round(h * scaleFactor);
+    ctx.drawImage(img, x, y, w, h, 0, 0, ctx.canvas.width, ctx.canvas.height);
+    let imageData = convertToBitmapArray(ctx.getImageData(0, 0, ctx.canvas.width, ctx.canvas.height).data);
+    printCppArray("dino_rest", imageData, ctx.canvas.width, ctx.canvas.height);
+    checkBitmap(imageData, ctx.canvas.width, ctx.canvas.height);
+}
+
+function checkBitmap(bitmap, w, h) {
+    ctx.clearRect(0, 0, w, h);
+
+    for (let i = 0; i < bitmap.length; i++) {
+        for (let j = 0; j < 8; j++) {
+            let idx = (i * 8 + j);
+            let x = (idx) % w;
+            let y = Math.floor(idx / w);
+            ctx.fillStyle = "black";
+            if (bitmap[i] & (0b1 << (8 - 1 - j)))
+                ctx.fillStyle = "white";
+            ctx.fillRect(x, y, 1, 1);
+        }
+    }
+}
+
+function convertToBitmapArray(arr) {
+    let ret = new Uint8Array((arr.length / 4) / 8);
+    let bits = 0;
+    let idx = 1;
+    let k = 0;
+    for (let i = 0; i < arr.length; i += 4) {
+        let r = arr[i];
+        let g = arr[i + 1];
+        let b = arr[i + 2];
+        if (idx == 8) {
+            idx = 1;
+            ret[k] = bits;
+            bits = 0;
+            k++;
+        } else {
+            idx++;
+            bits |= ((activation((r + g + b) / 3)) << ((8 - (idx - 1))));
+        }
+    }
+    return ret;
+}
+
+function activation(val) {
+    if (val >= 150) return 1;
+}
+
+function printCppArray(variableName, arr, w, h) {
+    let str = `const uint8_t  ${variableName}[] = {`;
+
+    for (let i = 0; i < arr.length; i++) {
+        // console.log(arr[i]);
+        str += `0x${(arr[i]).toString(16).toUpperCase()}`;
+        if ((i + 1) != arr.length) {
+            str += ',';
+        }
+    }
+
+    str += `}; // w=${w}  h=${h}`;
+
+    console.log(str);
+}
+
+function $(el) {
+    return document.querySelector(el);
+}
